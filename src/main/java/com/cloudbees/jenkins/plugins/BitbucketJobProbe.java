6b2cfd24cc8ebe46a185f1d6e1a9cfa389533ee1
jschoedt <jks@configit.com>
2016-02-06 14:15:29 +0100

    Changed keys for payload and added better logging

--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketJobProbe.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketJobProbe.java
@@ -1,125 +1,128 @@
 package com.cloudbees.jenkins.plugins;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
+
+import com.google.common.base.Objects;
+
 import hudson.model.Job;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.GitStatus;
 import hudson.plugins.mercurial.MercurialSCM;
 import hudson.scm.SCM;
 import hudson.security.ACL;
-
-import java.net.URISyntaxException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import hudson.triggers.Trigger;
 import jenkins.model.Jenkins;
-
 import jenkins.model.ParameterizedJobMixIn;
 import jenkins.triggers.SCMTriggerItem;
-import org.acegisecurity.context.SecurityContext;
-import org.acegisecurity.context.SecurityContextHolder;
-import org.eclipse.jgit.transport.RemoteConfig;
-import org.eclipse.jgit.transport.URIish;
-import com.google.common.base.Objects;
 
 public class BitbucketJobProbe {
 
-    @Deprecated
-    public void triggerMatchingJobs(String user, String url, String scm) {
-        triggerMatchingJobs(user, url, scm, "");
-    }
+	@Deprecated
+	public void triggerMatchingJobs(String user, String url, String scm) {
+		triggerMatchingJobs(user, url, scm, "");
+	}
 
-    public void triggerMatchingJobs(String user, String url, String scm, String payload) {
-        if ("git".equals(scm) || "hg".equals(scm)) {
-            SecurityContext old = Jenkins.getInstance().getACL().impersonate(ACL.SYSTEM);
-            try {
-                URIish remote = new URIish(url);
-                for (Job<?,?> job : Jenkins.getInstance().getAllItems(Job.class)) {
-                    BitBucketTrigger bTrigger = null;
-                    LOGGER.log(Level.FINE, "Considering candidate job {0}", job.getName());
+	public void triggerMatchingJobs(String user, String url, String scm, String payload) {
+		if ("git".equals(scm) || "hg".equals(scm)) {
+			SecurityContext old = Jenkins.getInstance().getACL().impersonate(ACL.SYSTEM);
+			try {
+				URIish remote = new URIish(url);
+				for (Job<?, ?> job : Jenkins.getInstance().getAllItems(Job.class)) {
+					BitBucketTrigger bTrigger = null;
+					LOGGER.log(Level.FINE, "Considering candidate job {0}", job.getName());
 
-                    if (job instanceof ParameterizedJobMixIn.ParameterizedJob) {
-                        ParameterizedJobMixIn.ParameterizedJob pJob = (ParameterizedJobMixIn.ParameterizedJob) job;
-                        for (Trigger trigger : pJob.getTriggers().values()) {
-                            if (trigger instanceof BitBucketTrigger) {
-                                bTrigger = (BitBucketTrigger) trigger;
-                                break;
-                            }
-                        }
-                    }
-                    if (bTrigger != null) {
-                        LOGGER.log(Level.FINE, "Considering to poke {0}", job.getFullDisplayName());
-                        SCMTriggerItem item = SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(job);
-                        List<SCM> scmTriggered = new ArrayList<SCM>();
-                        for (SCM scmTrigger : item.getSCMs()) {
-                            if (match(scmTrigger, remote) && !hasBeenTriggered(scmTriggered, scmTrigger)) {
-                                LOGGER.log(Level.INFO, "Triggering BitBucket job {0}", job.getName());
-                                scmTriggered.add(scmTrigger);
-                                bTrigger.onPost(user, payload);
-                            } else LOGGER.log(Level.FINE, "{0} SCM doesn't match remote repo {1}", new Object[]{job.getName(), remote});
-                        }
-                    } else
-                        LOGGER.log(Level.FINE, "{0} hasn't BitBucketTrigger set", job.getName());
-                }
-            } catch (URISyntaxException e) {
-                LOGGER.log(Level.WARNING, "Invalid repository URL {0}", url);
-            } finally {
-                SecurityContextHolder.setContext(old);
-            }
+					if (job instanceof ParameterizedJobMixIn.ParameterizedJob) {
+						ParameterizedJobMixIn.ParameterizedJob pJob = (ParameterizedJobMixIn.ParameterizedJob) job;
+						for (Trigger trigger : pJob.getTriggers().values()) {
+							if (trigger instanceof BitBucketTrigger) {
+								bTrigger = (BitBucketTrigger) trigger;
+								break;
+							}
+						}
+					}
+					if (bTrigger != null) {
+						LOGGER.log(Level.FINE, "Considering to poke {0}", job.getFullDisplayName());
+						SCMTriggerItem item = SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(job);
+						List<SCM> scmTriggered = new ArrayList<SCM>();
+						Collection<? extends SCM> scMs = item.getSCMs();
+						if (scMs.isEmpty())
+							LOGGER.log(Level.INFO, "No SCM is set up for: {0}", job.getName());
+						for (SCM scmTrigger : scMs) {
+							if (match(scmTrigger, remote) && !hasBeenTriggered(scmTriggered, scmTrigger)) {
+								LOGGER.log(Level.INFO, "Triggering BitBucket job {0}", job.getName());
+								scmTriggered.add(scmTrigger);
+								bTrigger.onPost(user, payload);
+							} else
+								LOGGER.log(Level.INFO, job.getName() + " SCM doesn't match remote repo " + remote);
+						}
+					} else
+						LOGGER.log(Level.INFO, "{0} hasn't BitBucketTrigger set", job.getName());
+				}
+			} catch (URISyntaxException e) {
+				LOGGER.log(Level.WARNING, "Invalid repository URL {0}", url);
+			} finally {
+				SecurityContextHolder.setContext(old);
+			}
 
-        } else {
-            throw new UnsupportedOperationException("Unsupported SCM type " + scm);
-        }
-    }
+		} else {
+			throw new UnsupportedOperationException("Unsupported SCM type " + scm);
+		}
+	}
 
-    private boolean hasBeenTriggered(List<SCM> scmTriggered, SCM scmTrigger) {
-        for (SCM scm : scmTriggered) {
-            if (scm.equals(scmTrigger)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private boolean hasBeenTriggered(List<SCM> scmTriggered, SCM scmTrigger) {
+		for (SCM scm : scmTriggered) {
+			if (scm.equals(scmTrigger)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private boolean match(SCM scm, URIish url) {
-        if (scm instanceof GitSCM) {
-            for (RemoteConfig remoteConfig : ((GitSCM) scm).getRepositories()) {
-                for (URIish urIish : remoteConfig.getURIs()) {
-                    if (GitStatus.looselyMatches(urIish, url)) {
-                        return true;
-                    }
-                }
-            }
-        } else if (scm instanceof MercurialSCM) {
-            try {
-                URI hgUri = new URI(((MercurialSCM) scm).getSource());
-                String remote = url.toString();
-                if (looselyMatches(hgUri, remote)) {
-                    return true;
-                }
-            } catch (URISyntaxException ex) {
-                LOGGER.log(Level.SEVERE, "Could not parse jobSource uri: {0} ", ex);
-            }
-        }
-        return false;
-    }
+	private boolean match(SCM scm, URIish url) {
+		if (scm instanceof GitSCM) {
+			for (RemoteConfig remoteConfig : ((GitSCM) scm).getRepositories()) {
+				for (URIish urIish : remoteConfig.getURIs()) {
+					if (GitStatus.looselyMatches(urIish, url)) {
+						return true;
+					}
+				}
+			}
+		} else if (scm instanceof MercurialSCM) {
+			try {
+				URI hgUri = new URI(((MercurialSCM) scm).getSource());
+				String remote = url.toString();
+				if (looselyMatches(hgUri, remote)) {
+					return true;
+				}
+			} catch (URISyntaxException ex) {
+				LOGGER.log(Level.SEVERE, "Could not parse jobSource uri: {0} ", ex);
+			}
+		}
+		return false;
+	}
 
-    private boolean looselyMatches(URI notifyUri, String repository) {
-        boolean result = false;
-        try {
-            URI repositoryUri = new URI(repository);
-            result = Objects.equal(notifyUri.getHost(), repositoryUri.getHost())
-            && Objects.equal(notifyUri.getPath(), repositoryUri.getPath())
-            && Objects.equal(notifyUri.getQuery(), repositoryUri.getQuery());
-        } catch (URISyntaxException ex) {
-            LOGGER.log(Level.SEVERE, "Could not parse repository uri: {0}, {1}", new Object[]{repository, ex});
-        }
-        return result;
-    }
+	private boolean looselyMatches(URI notifyUri, String repository) {
+		boolean result = false;
+		try {
+			URI repositoryUri = new URI(repository);
+			result = Objects.equal(notifyUri.getHost(), repositoryUri.getHost()) && Objects.equal(notifyUri.getPath(), repositoryUri.getPath()) && Objects.equal(notifyUri.getQuery(), repositoryUri.getQuery());
+		} catch (URISyntaxException ex) {
+			LOGGER.log(Level.SEVERE, "Could not parse repository uri: {0}, {1}", new Object[] { repository, ex });
+		}
+		return result;
+	}
 
-    private static final Logger LOGGER = Logger.getLogger(BitbucketJobProbe.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(BitbucketJobProbe.class.getName());
 
 }
--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
@@ -9,99 +9,102 @@ import net.sf.json.JSONObject;
 
 public class BitbucketPayloadProcessor {
 
-    private final BitbucketJobProbe probe;
+	private final BitbucketJobProbe probe;
 
-    public BitbucketPayloadProcessor(BitbucketJobProbe probe) {
-        this.probe = probe;
-    }
+	public BitbucketPayloadProcessor(BitbucketJobProbe probe) {
+		this.probe = probe;
+	}
 
-    public BitbucketPayloadProcessor() {
-        this(new BitbucketJobProbe());
-    }
+	public BitbucketPayloadProcessor() {
+		this(new BitbucketJobProbe());
+	}
 
-    public void processPayload(JSONObject payload, HttpServletRequest request) {
-        if ("Bitbucket-Webhooks/2.0".equals(request.getHeader("user-agent"))) {
-            if ("repo:push".equals(request.getHeader("x-event-key"))) {
-                LOGGER.log(Level.INFO, "Processing new Webhooks payload");
-                processWebhookPayload(payload);
-            }
-        } else {
-            LOGGER.log(Level.INFO, "Processing old POST service payload");
-            processPostServicePayload(payload);
-        }
-    }
+	public void processPayload(JSONObject payload, HttpServletRequest request) {
+		if ("Bitbucket-Webhooks/2.0".equals(request.getHeader("user-agent"))) {
+			if ("repo:push".equals(request.getHeader("x-event-key"))) {
+				LOGGER.log(Level.INFO, "Processing new Webhooks payload");
+				processWebhookPayload(payload);
+			}
+		} else {
+			LOGGER.log(Level.INFO, "Processing old POST service payload");
+			processPostServicePayload(payload);
+		}
+	}
 
-    private void processWebhookPayload(JSONObject payload) {
-        if (payload.has("repository")) {
-            JSONObject repo = payload.getJSONObject("repository");
-            LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
+	private void processWebhookPayload(JSONObject payload) {
+		if (payload.has("repository")) {
+			JSONObject repo = payload.getJSONObject("repository");
+			LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
 
-            String user = payload.getJSONObject("actor").getString("username");
-            String url = repo.getJSONObject("links").getJSONObject("html").getString("href");
-            String scm = repo.has("scm") ? repo.getString("scm") : "git";
+			String user = payload.getJSONObject("actor").getString("username");
+			String url = repo.getJSONObject("links").getJSONObject("html").getString("href");
+			String scm = repo.has("scm") ? repo.getString("scm") : "git";
 
-            probe.triggerMatchingJobs(user, url, scm, payload.toString());
-        } else if (payload.has("scm")) {
-            LOGGER.log(Level.INFO, "Received commit hook notification for hg: {0}", payload);
-            String user = payload.getJSONObject("owner").getString("username");
-            String url = payload.getJSONObject("links").getJSONObject("html").getString("href");
-            String scm = payload.has("scm") ? payload.getString("scm") : "hg";
+			probe.triggerMatchingJobs(user, url, scm, payload.toString());
+		} else if (payload.has("scm")) {
+			LOGGER.log(Level.INFO, "Received commit hook notification for hg: {0}", payload);
+			String user = payload.getJSONObject("owner").getString("username");
+			String url = payload.getJSONObject("links").getJSONObject("html").getString("href");
+			String scm = payload.has("scm") ? payload.getString("scm") : "hg";
 
-            probe.triggerMatchingJobs(user, url, scm, payload.toString());
-        }
+			probe.triggerMatchingJobs(user, url, scm, payload.toString());
+		}
 
-    }
+	}
 
-/*
-{
-    "canon_url": "https://bitbucket.org",
-    "commits": [
-        {
-            "author": "marcus",
-            "branch": "master",
-            "files": [
-                {
-                    "file": "somefile.py",
-                    "type": "modified"
-                }
-            ],
-            "message": "Added some more things to somefile.py\n",
-            "node": "620ade18607a",
-            "parents": [
-                "702c70160afc"
-            ],
-            "raw_author": "Marcus Bertrand <marcus@somedomain.com>",
-            "raw_node": "620ade18607ac42d872b568bb92acaa9a28620e9",
-            "revision": null,
-            "size": -1,
-            "timestamp": "2012-05-30 05:58:56",
-            "utctimestamp": "2012-05-30 03:58:56+00:00"
-        }
-    ],
-    "repository": {
-        "absolute_url": "/marcus/project-x/",
-        "fork": false,
-        "is_private": true,
-        "name": "Project X",
-        "owner": "marcus",
-        "scm": "git",
-        "slug": "project-x",
-        "website": "https://atlassian.com/"
-    },
-    "user": "marcus"
-}
-*/
-    private void processPostServicePayload(JSONObject payload) {
-        JSONObject repo = payload.getJSONObject("repository");
-        LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
+	/*
+	{
+	"canon_url": "https://bitbucket.org",
+	"commits": [
+	    {
+	        "author": "marcus",
+	        "branch": "master",
+	        "files": [
+	            {
+	                "file": "somefile.py",
+	                "type": "modified"
+	            }
+	        ],
+	        "message": "Added some more things to somefile.py\n",
+	        "node": "620ade18607a",
+	        "parents": [
+	            "702c70160afc"
+	        ],
+	        "raw_author": "Marcus Bertrand <marcus@somedomain.com>",
+	        "raw_node": "620ade18607ac42d872b568bb92acaa9a28620e9",
+	        "revision": null,
+	        "size": -1,
+	        "timestamp": "2012-05-30 05:58:56",
+	        "utctimestamp": "2012-05-30 03:58:56+00:00"
+	    }
+	],
+	"repository": {
+	    "absolute_url": "/marcus/project-x/",
+	    "fork": false,
+	    "is_private": true,
+	    "name": "Project X",
+	    "owner": "marcus",
+	    "scm": "git",
+	    "slug": "project-x",
+	    "website": "https://atlassian.com/"
+	},
+	"user": "marcus"
+	}
+	*/
+	private void processPostServicePayload(JSONObject payload) {
+		JSONObject repo = payload.getJSONObject("repository");
+		LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
 
-        String user = payload.getString("user");
-        String url = payload.getString("canon_url") + repo.getString("absolute_url");
-        String scm = repo.getString("scm");
+		JSONObject actor = payload.getJSONObject("actor");
+		LOGGER.log(Level.INFO, "Received commit hook notification from actor {0}", actor);
 
-        probe.triggerMatchingJobs(user, url, scm, payload.toString());
-    }
+		String user = actor.getString("username");
+		String url = repo.getJSONObject("links").getJSONObject("html").getString("href");
+		String scm = repo.getString("scm");
 
-    private static final Logger LOGGER = Logger.getLogger(BitbucketPayloadProcessor.class.getName());
+		probe.triggerMatchingJobs(user, url, scm, payload.toString());
+	}
+
+	private static final Logger LOGGER = Logger.getLogger(BitbucketPayloadProcessor.class.getName());
 
 }
