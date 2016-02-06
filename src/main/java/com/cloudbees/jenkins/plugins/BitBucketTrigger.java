7d2ae7e7cae44f3f984cb0343ea81a7e9bc0d554
jschoedt <jks@configit.com>
2016-02-06 15:02:56 +0100

    Removed change check for new commits as it was using last build branch which will not work if hook message is from another branch

--- src/main/java/com/cloudbees/jenkins/plugins/BitBucketTrigger.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitBucketTrigger.java
@@ -1,5 +1,16 @@
 package com.cloudbees.jenkins.plugins;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.jelly.XMLOutput;
+import org.kohsuke.stapler.DataBoundConstructor;
+
 import hudson.Extension;
 import hudson.Util;
 import hudson.console.AnnotatedLargeText;
@@ -11,178 +22,134 @@ import hudson.model.Job;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
 import hudson.util.SequentialExecutionQueue;
-import hudson.util.StreamTaskListener;
 import jenkins.model.ParameterizedJobMixIn;
 import jenkins.triggers.SCMTriggerItem;
-import org.apache.commons.jelly.XMLOutput;
-import org.kohsuke.stapler.DataBoundConstructor;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.text.DateFormat;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 public class BitBucketTrigger extends Trigger<Job<?, ?>> {
 
-    @DataBoundConstructor
-    public BitBucketTrigger() {
-    }
+	@DataBoundConstructor
+	public BitBucketTrigger() {
+	}
 
-    /**
-     * Called when a POST is made.
-     */
-    @Deprecated
-    public void onPost(String triggeredByUser) {
-        onPost(triggeredByUser, "");
-    }
+	/**
+	 * Called when a POST is made.
+	 */
+	@Deprecated
+	public void onPost(String triggeredByUser) {
+		onPost(triggeredByUser, "");
+	}
 
-    /**
-     * Called when a POST is made.
-     */
-    public void onPost(String triggeredByUser, final String payload) {
-        final String pushBy = triggeredByUser;
-        getDescriptor().queue.execute(new Runnable() {
-            private boolean runPolling() {
-                try {
-                    StreamTaskListener listener = new StreamTaskListener(getLogFile());
-                    try {
-                        PrintStream logger = listener.getLogger();
-                        long start = System.currentTimeMillis();
-                        logger.println("Started on "+ DateFormat.getDateTimeInstance().format(new Date()));
-                        boolean result = SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(job).poll(listener).hasChanges();
-                        logger.println("Done. Took "+ Util.getTimeSpanString(System.currentTimeMillis()-start));
-                        if(result)
-                            logger.println("Changes found");
-                        else
-                            logger.println("No changes");
-                        return result;
-                    } catch (Error e) {
-                        e.printStackTrace(listener.error("Failed to record SCM polling"));
-                        LOGGER.log(Level.SEVERE,"Failed to record SCM polling",e);
-                        throw e;
-                    } catch (RuntimeException e) {
-                        e.printStackTrace(listener.error("Failed to record SCM polling"));
-                        LOGGER.log(Level.SEVERE,"Failed to record SCM polling",e);
-                        throw e;
-                    } finally {
-                        listener.close();
-                    }
-                } catch (IOException e) {
-                    LOGGER.log(Level.SEVERE,"Failed to record SCM polling",e);
-                }
-                return false;
-            }
+	/**
+	 * Called when a POST is made.
+	 */
+	public void onPost(String triggeredByUser, final String payload) {
+		final String pushBy = triggeredByUser;
+		getDescriptor().queue.execute(new Runnable() {
+			@Override
+			public void run() {
+				String name = " #" + job.getNextBuildNumber();
+				BitBucketPushCause cause;
+				try {
+					cause = new BitBucketPushCause(getLogFile(), pushBy);
+				} catch (IOException e) {
+					LOGGER.log(Level.WARNING, "Failed to parse the polling log", e);
+					cause = new BitBucketPushCause(pushBy);
+				}
+				ParameterizedJobMixIn pJob = new ParameterizedJobMixIn() {
+					@Override
+					protected Job asJob() {
+						return job;
+					}
+				};
+				BitBucketPayload bitBucketPayload = new BitBucketPayload(payload);
+				pJob.scheduleBuild2(5, new CauseAction(cause), bitBucketPayload);
+				if (pJob.scheduleBuild(cause)) {
+					LOGGER.info("SCM changes detected in " + job.getName() + ". Triggering " + name);
+				} else {
+					LOGGER.info("SCM changes detected in " + job.getName() + ". Job is already in the queue");
+				}
+			}
 
-            public void run() {
-                if (runPolling()) {
-                    String name = " #"+job.getNextBuildNumber();
-                    BitBucketPushCause cause;
-                    try {
-                        cause = new BitBucketPushCause(getLogFile(), pushBy);
-                    } catch (IOException e) {
-                        LOGGER.log(Level.WARNING, "Failed to parse the polling log",e);
-                        cause = new BitBucketPushCause(pushBy);
-                    }
-                    ParameterizedJobMixIn pJob = new ParameterizedJobMixIn() {
-                        @Override protected Job asJob() {
-                            return job;
-                        }
-                    };
-                    BitBucketPayload bitBucketPayload = new BitBucketPayload(payload);
-                    pJob.scheduleBuild2(5, new CauseAction(cause), bitBucketPayload);
-                    if (pJob.scheduleBuild(cause)) {
-                        LOGGER.info("SCM changes detected in "+ job.getName()+". Triggering "+ name);
-                    } else {
-                        LOGGER.info("SCM changes detected in "+ job.getName()+". Job is already in the queue");
-                    }
-                }
-            }
+		});
+	}
 
-        });
-    }
+	@Override
+	public Collection<? extends Action> getProjectActions() {
+		return Collections.singleton(new BitBucketWebHookPollingAction());
+	}
 
-    @Override
-    public Collection<? extends Action> getProjectActions() {
-        return Collections.singleton(new BitBucketWebHookPollingAction());
-    }
+	/**
+	 * Returns the file that records the last/current polling activity.
+	 */
+	public File getLogFile() {
+		return new File(job.getRootDir(), "bitbucket-polling.log");
+	}
 
-    /**
-     * Returns the file that records the last/current polling activity.
-     */
-    public File getLogFile() {
-        return new File(job.getRootDir(),"bitbucket-polling.log");
-    }
+	/**
+	 * Check if "bitbucket-polling.log" already exists to initialize it
+	 */
+	public boolean IsLogFileInitialized() {
+		File file = new File(job.getRootDir(), "bitbucket-polling.log");
+		return file.exists();
+	}
 
-    /**
-     * Check if "bitbucket-polling.log" already exists to initialize it
-     */
-    public boolean IsLogFileInitialized() {
-        File file = new File(job.getRootDir(),"bitbucket-polling.log");
-        return file.exists();
-    }
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
 
-    @Override
-    public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
-    }
+	/**
+	 * Action object for {@link Project}. Used to display the polling log.
+	 */
+	public final class BitBucketWebHookPollingAction implements Action {
+		public Job<?, ?> getOwner() {
+			return job;
+		}
 
-    /**
-     * Action object for {@link Project}. Used to display the polling log.
-     */
-    public final class BitBucketWebHookPollingAction implements Action {
-        public Job<?,?> getOwner() {
-            return job;
-        }
+		@Override
+		public String getIconFileName() {
+			return "clipboard.png";
+		}
 
-        public String getIconFileName() {
-            return "clipboard.png";
-        }
+		@Override
+		public String getDisplayName() {
+			return "BitBucket Hook Log";
+		}
 
-        public String getDisplayName() {
-            return "BitBucket Hook Log";
-        }
+		@Override
+		public String getUrlName() {
+			return "BitBucketPollLog";
+		}
 
-        public String getUrlName() {
-            return "BitBucketPollLog";
-        }
+		public String getLog() throws IOException {
+			return Util.loadFile(getLogFile());
+		}
 
-        public String getLog() throws IOException {
-            return Util.loadFile(getLogFile());
-        }
+		/**
+		 * Writes the annotated log to the given output.
+		 */
+		public void writeLogTo(XMLOutput out) throws IOException {
+			new AnnotatedLargeText<BitBucketWebHookPollingAction>(getLogFile(), Charset.defaultCharset(), true, this).writeHtmlTo(0, out.asWriter());
+		}
+	}
 
-        /**
-         * Writes the annotated log to the given output.
-         */
-        public void writeLogTo(XMLOutput out) throws IOException {
-            new AnnotatedLargeText<BitBucketWebHookPollingAction>(getLogFile(), Charset.defaultCharset(),true,this).writeHtmlTo(0,out.asWriter());
-        }
-    }
+	@Extension
+	public static class DescriptorImpl extends TriggerDescriptor {
+		private transient final SequentialExecutionQueue queue = new SequentialExecutionQueue(Hudson.MasterComputer.threadPoolForRemoting);
 
-    @Extension
-    public static class DescriptorImpl extends TriggerDescriptor {
-        private transient final SequentialExecutionQueue queue = new SequentialExecutionQueue(Hudson.MasterComputer.threadPoolForRemoting);
+		@Override
+		public boolean isApplicable(Item item) {
+			return item instanceof Job && SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(item) != null && item instanceof ParameterizedJobMixIn.ParameterizedJob;
+		}
 
-        @Override
-        public boolean isApplicable(Item item) {
-            return item instanceof Job && SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(item) != null
-                    && item instanceof ParameterizedJobMixIn.ParameterizedJob;
-        }
+		@Override
+		public String getDisplayName() {
+			return "Build when a change is pushed to BitBucket";
+		}
+	}
 
-        @Override
-        public String getDisplayName() {
-            return "Build when a change is pushed to BitBucket";
-        }
-    }
-
-    private static final Logger LOGGER = Logger.getLogger(BitBucketTrigger.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(BitBucketTrigger.class.getName());
 }
