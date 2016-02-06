99959bb874a26322364887898df1d9047c15a266
jschoedt <jks@configit.com>
2016-02-06 15:03:32 +0100

    Added BRANCH env variable

--- src/main/java/com/cloudbees/jenkins/plugins/BitBucketPayload.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitBucketPayload.java
@@ -1,13 +1,15 @@
 package com.cloudbees.jenkins.plugins;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Nonnull;
+
 import hudson.EnvVars;
 import hudson.model.AbstractBuild;
 import hudson.model.EnvironmentContributingAction;
 import hudson.model.InvisibleAction;
-
-import javax.annotation.Nonnull;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import net.sf.json.JSONObject;
 
 /**
  * Inject the payload received by BitBucket into the build through $BITBUCKET_PAYLOAD so it can be processed
@@ -15,23 +17,35 @@ import java.util.logging.Logger;
  * @version 1.1.5
  */
 public class BitBucketPayload extends InvisibleAction implements EnvironmentContributingAction {
-    private final @Nonnull String payload;
+	private final @Nonnull String payload;
 
-    public BitBucketPayload(@Nonnull String payload) {
-        this.payload = payload;
-    }
+	public BitBucketPayload(@Nonnull String payload) {
+		this.payload = payload;
+	}
 
-    @Nonnull
-    public String getPayload() {
-        return payload;
-    }
+	@Nonnull
+	public String getPayload() {
+		return payload;
+	}
 
-    @Override
-    public void buildEnvVars(AbstractBuild<?, ?> abstractBuild, EnvVars envVars) {
-        final String payload = getPayload();
-        LOGGER.log(Level.FINEST, "Injecting BITBUCKET_PAYLOAD: {0}", payload);
-        envVars.put("BITBUCKET_PAYLOAD", payload);
-    }
+	@Override
+	public void buildEnvVars(AbstractBuild<?, ?> abstractBuild, EnvVars envVars) {
+		final String payload = getPayload();
+		LOGGER.log(Level.FINEST, "Injecting BITBUCKET_PAYLOAD: {0}", payload);
+		envVars.put("BITBUCKET_PAYLOAD", payload);
+		envVars.put("BRANCH", getBranchName(payload));
+	}
 
-    private static final Logger LOGGER = Logger.getLogger(BitBucketPayload.class.getName());
+	private String getBranchName(String body) {
+		String result = "";
+		try {
+			JSONObject payload = JSONObject.fromObject(body);
+			result = payload.getJSONObject("push").getJSONArray("changes").getJSONObject(0).getJSONObject("old").getString("name");
+		} catch (Exception e) {
+			LOGGER.log(Level.FINEST, "No branch name found");
+		}
+		return result;
+	}
+
+	private static final Logger LOGGER = Logger.getLogger(BitBucketPayload.class.getName());
 }
