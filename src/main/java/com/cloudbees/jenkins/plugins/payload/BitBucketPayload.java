3f7632a7b6ef4d41cab8b03ef97fe80173b777c6
Shyri Villar <igs.villar@gmail.com>
2016-03-11 14:57:22 +0100

    Move BitbucketPayload

---
+++ src/main/java/com/cloudbees/jenkins/plugins/payload/BitBucketPayload.java
@@ -0,0 +1,66 @@
+package com.cloudbees.jenkins.plugins.payload;
+
+import hudson.EnvVars;
+import hudson.model.AbstractBuild;
+import hudson.model.EnvironmentContributingAction;
+import hudson.model.InvisibleAction;
+import net.sf.json.JSONObject;
+
+import javax.annotation.Nonnull;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Inject the payload received by BitBucket into the build through $BITBUCKET_PAYLOAD so it can be processed
+ * @since January 9, 2016
+ * @version 1.1.5
+ */
+public class BitBucketPayload extends InvisibleAction implements EnvironmentContributingAction {
+    private final @Nonnull JSONObject payload;
+    private String scm;
+    private String user;
+    private String scmUrl;
+
+    public BitBucketPayload(@Nonnull JSONObject payload) {
+        this.payload = payload;
+        JSONObject repository = payload.getJSONObject("repository");
+        JSONObject actor = payload.getJSONObject("actor");
+
+        this.user = actor.getString("username");
+        this.scm = repository.has("scm") ? repository.getString("scm") : "git";
+        this.scmUrl = repository.getJSONObject("links").getJSONObject("html").getString("href");
+    }
+
+    @Nonnull
+    public JSONObject getPayload() {
+        return payload;
+    }
+
+    public String getScm() {
+        return scm;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getScmUrl() {
+        return scmUrl;
+    }
+
+    @Override
+    public void buildEnvVars(AbstractBuild<?, ?> abstractBuild, EnvVars envVars) {
+        envVars.put("BITBUCKET_PAYLOAD", payload.toString());
+
+        JSONObject jsonObject = JSONObject.fromObject(payload);
+        JSONObject pullRequest = jsonObject.getJSONObject("pullrequest");
+        JSONObject source = pullRequest.getJSONObject("source");
+
+        String branch = source.getJSONObject("branch").getString("name");
+        envVars.put("BITBUCKET_BRANCH", branch);
+
+        LOGGER.log(Level.FINEST, "Injecting BITBUCKET_PAYLOAD: {0}", payload);
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(BitBucketPayload.class.getName());
+}
