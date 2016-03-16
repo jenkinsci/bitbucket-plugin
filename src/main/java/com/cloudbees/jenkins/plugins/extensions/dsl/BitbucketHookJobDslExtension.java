73d1f4c4562b175b9ee7eeda702fdb2b8dc0b09e
Shyri Villar <igs.villar@gmail.com>
2016-03-16 08:25:41 +0100

    Try to restore DSL

--- src/main/java/com/cloudbees/jenkins/plugins/extensions/dsl/BitbucketHookJobDslExtension.java
+++ src/main/java/com/cloudbees/jenkins/plugins/extensions/dsl/BitbucketHookJobDslExtension.java
@@ -1,14 +1,20 @@
 package com.cloudbees.jenkins.plugins.extensions.dsl;
 
+import com.cloudbees.jenkins.plugins.BitBucketTrigger;
+import com.cloudbees.jenkins.plugins.filter.BitbucketTriggerFilter;
 import hudson.Extension;
+import javaposse.jobdsl.dsl.helpers.triggers.TriggerContext;
 import javaposse.jobdsl.plugin.ContextExtensionPoint;
+import javaposse.jobdsl.plugin.DslExtensionMethod;
+
+import java.util.List;
 
 @Extension(optional = true)
 public class BitbucketHookJobDslExtension extends ContextExtensionPoint {
-//    @DslExtensionMethod(context = TriggerContext.class)
-//    public Object bitbucketPush(Runnable closure) {
-//        return new BitBucketTrigger();
-//    }
+    @DslExtensionMethod(context = TriggerContext.class)
+    public Object bitbucketPush(List<BitbucketTriggerFilter> triggers, Runnable closure) {
+        return new BitBucketTrigger(triggers);
+    }
 }
 
 
