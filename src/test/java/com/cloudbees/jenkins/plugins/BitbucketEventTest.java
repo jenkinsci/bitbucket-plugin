8234a15886ef393e8583e4aec9dbf1b06e683efa
Félix Belzunce Arcos <fbelzunc@gmail.com>
2016-08-09 14:55:35 +0200

    Test clean-up

--- src/test/java/com/cloudbees/jenkins/plugins/BitbucketEventTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/BitbucketEventTest.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 CloudBees, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.plugins;
 
 import org.junit.Test;
@@ -9,9 +33,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-/**
- * Created by Shyri Villar on 11/03/2016.
- */
 
 @RunWith(MockitoJUnitRunner.class)
 public class BitbucketEventTest {
@@ -97,4 +118,5 @@ public class BitbucketEventTest {
 
         return bitbucketEvent;
     }
+
 }
--- src/test/java/com/cloudbees/jenkins/plugins/processor/RepositoryPayloadProcessorTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/processor/RepositoryPayloadProcessorTest.java
@@ -1,9 +1,30 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 CloudBees, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.plugins.processor;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,7 +52,7 @@ public class RepositoryPayloadProcessorTest {
     private RepositoryPayloadProcessor repositoryPayloadProcessor;
 
     @Test
-    public void testProcessPullRequestApprovalWebhookGit() {
+    public void testRepositoryPushWebhookGit() {
         // Set headers so that payload processor will parse as new Webhook payload
         when(request.getHeader("user-agent")).thenReturn("Bitbucket-Webhooks/2.0");
         when(request.getHeader("x-event-key")).thenReturn("repo:push");
@@ -42,7 +63,6 @@ public class RepositoryPayloadProcessorTest {
         BitbucketEvent bitbucketEvent = new BitbucketEvent(request.getHeader("x-event-key"));
 
         repositoryPayloadProcessor = new RepositoryPayloadProcessor(probe, bitbucketEvent);
-
 
         JSONObject payload = new JSONObject()
                 .element("actor", new JSONObject()
@@ -60,8 +80,20 @@ public class RepositoryPayloadProcessorTest {
         assertEquals(payload, payloadCaptor.getValue().getPayload());
     }
 
+    @Test(expected = UnsupportedOperationException.class)
+    public void testRepositoryAnyWebhookGit() {
+        // Set headers so that payload processor will parse as new Webhook payload
+        when(request.getHeader("user-agent")).thenReturn("Bitbucket-Webhooks/2.0");
+        when(request.getHeader("x-event-key")).thenReturn("repo:any");
+
+        String user = "test_user";
+        String url = "https://bitbucket.org/test_user/test_repo";
+
+        BitbucketEvent bitbucketEvent = new BitbucketEvent(request.getHeader("x-event-key"));
+    }
+
     @Test
-    public void testProcessPullRequestApprovalWebhookHg() {
+    public void testRepositoryPushWebhookHg() {
         // Set headers so that payload processor will parse as new Webhook payload
         when(request.getHeader("user-agent")).thenReturn("Bitbucket-Webhooks/2.0");
         when(request.getHeader("x-event-key")).thenReturn("repo:push");
@@ -88,4 +120,17 @@ public class RepositoryPayloadProcessorTest {
         assertEquals(bitbucketEvent, eventCaptor.getValue());
         assertEquals(hgLoad, payloadCaptor.getValue().getPayload());
     }
+
+    @Test
+    public void testRepositoryAnyWebhookHg() {
+        // Set headers so that payload processor will parse as new Webhook payload
+        when(request.getHeader("user-agent")).thenReturn("Bitbucket-Webhooks/2.0");
+        when(request.getHeader("x-event-key")).thenReturn("repo:push");
+
+        String user = "test_user";
+        String url = "https://bitbucket.org/test_user/test_repo";
+
+        BitbucketEvent bitbucketEvent = new BitbucketEvent(request.getHeader("x-event-key"));
+    }
+
 }
