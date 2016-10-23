7bc207d6cb3c1fe5e7957bb390d03158c1d6cc17
Gabriel Martinez <gabriel@proquo.com.au>
2016-10-23 12:55:57 +1100

    more style fixes

--- src/test/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessorTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessorTest.java
@@ -20,10 +20,8 @@ import org.mockito.verification.VerificationMode;
 @RunWith(MockitoJUnitRunner.class)
 public class BitbucketPayloadProcessorTest {
 
-    @Mock
-    private HttpServletRequest request;
-    @Mock
-    private BitbucketJobProbe probe;
+    @Mock private HttpServletRequest request;
+    @Mock private BitbucketJobProbe probe;
 
     private BitbucketPayloadProcessor payloadProcessor;
 
@@ -40,7 +38,7 @@ public class BitbucketPayloadProcessorTest {
         testProcessWebhookPayload(commitMessage, times(1));
 
         // Test all valid flags
-        String[] flags = { "[ci skip]", "[skip ci]", "--skip-ci" };
+        String[] flags = {"[ci skip]", "[skip ci]", "--skip-ci"};
 
         for (String flag : flags) {
             testProcessWebhookPayload(commitMessage + flag, never());
@@ -48,8 +46,7 @@ public class BitbucketPayloadProcessorTest {
     }
 
     private void testProcessWebhookPayload(String message, VerificationMode verifMode) {
-        // Set headers so that payload processor will parse as new Webhook
-        // payload
+        // Set headers so that payload processor will parse as new Webhook payload
         when(request.getHeader("user-agent")).thenReturn("Bitbucket-Webhooks/2.0");
         when(request.getHeader("x-event-key")).thenReturn("repo:push");
 
@@ -57,16 +54,28 @@ public class BitbucketPayloadProcessorTest {
         String url = "https://bitbucket.org/test_user/test_repo";
 
         JSONArray commits = new JSONArray();
-        commits.add(new JSONObject().element("author", "jenkins ci").element("branch", "master").element("message",
-                message));
+        commits.add(new JSONObject()
+                .element("author", "jenkins ci")
+                .element("branch", "master")
+                .element("message", message));
 
-        JSONObject payload = new JSONObject().element("actor", new JSONObject().element("username", user))
-                .element("commits", commits).element("repository", new JSONObject().element("links",
-                        new JSONObject().element("html", new JSONObject().element("href", url))));
+        JSONObject payload = new JSONObject()
+            .element("actor", new JSONObject()
+                .element("username", user))
+            .element("commits", commits)
+            .element("repository", new JSONObject()
+                .element("links", new JSONObject()
+                    .element("html", new JSONObject()
+                        .element("href", url))));
 
-        JSONObject hgLoad = new JSONObject().element("scm", "hg")
-                .element("owner", new JSONObject().element("username", user)).element("commits", commits)
-                .element("links", new JSONObject().element("html", new JSONObject().element("href", url)));
+        JSONObject hgLoad = new JSONObject()
+            .element("scm", "hg")
+            .element("owner", new JSONObject()
+                .element("username", user))
+            .element("commits", commits)
+            .element("links", new JSONObject()
+                .element("html", new JSONObject()
+                    .element("href", url)));
 
         payloadProcessor.processPayload(payload, request);
 
@@ -79,18 +88,19 @@ public class BitbucketPayloadProcessorTest {
 
     @Test
     public void testProcessPostServicePayload() {
-        // Ensure header isn't set so that payload processor will parse as old
-        // POST service payload
+        // Ensure header isn't set so that payload processor will parse as old POST service payload
         when(request.getHeader("user-agent")).thenReturn(null);
-
-        JSONObject payload = new JSONObject().element("canon_url", "https://staging.bitbucket.org")
-                .element("user", "old_user").element("repository",
-                        new JSONObject().element("scm", "git").element("absolute_url", "/old_user/old_repo"));
+        
+        JSONObject payload = new JSONObject()
+            .element("canon_url", "https://staging.bitbucket.org")
+            .element("user", "old_user")
+            .element("repository", new JSONObject()
+                .element("scm", "git")
+                .element("absolute_url", "/old_user/old_repo"));
 
         payloadProcessor.processPayload(payload, request);
 
-        verify(probe).triggerMatchingJobs("old_user", "https://staging.bitbucket.org/old_user/old_repo", "git",
-                payload.toString());
+        verify(probe).triggerMatchingJobs("old_user", "https://staging.bitbucket.org/old_user/old_repo", "git", payload.toString());
     }
 
 }
