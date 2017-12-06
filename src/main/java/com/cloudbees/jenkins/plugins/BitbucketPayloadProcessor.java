f0bbeba23367daa7d8ea94e3b7351a7d543f1e7d
Felix Belzunce Arcos <fbelzuncearcos@cloudbees.com>
2017-12-06 13:55:12 +0100

    [FIXED JENKINS-28877] Adding BitBucket server webhook (#53)
    [JENKINS-28877] Adding BitBucket server webhook

--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
@@ -1,5 +1,7 @@
 package com.cloudbees.jenkins.plugins;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,6 +26,11 @@ public class BitbucketPayloadProcessor {
             if ("repo:push".equals(request.getHeader("x-event-key"))) {
                 LOGGER.log(Level.INFO, "Processing new Webhooks payload");
                 processWebhookPayload(payload);
+            }
+        } else if (payload.has("actor") && payload.has("repository")) {
+            if ("repo:push".equals(request.getHeader("x-event-key"))) {
+                LOGGER.log(Level.INFO, "Processing new Webhooks payload");
+                processWebhookPayloadBitBucketServer(payload);
             }
         } else {
             LOGGER.log(Level.INFO, "Processing old POST service payload");
@@ -50,6 +57,33 @@ public class BitbucketPayloadProcessor {
             probe.triggerMatchingJobs(user, url, scm, payload.toString());
         }
 
+    }
+
+    /**
+     * Payload processor for BitBucket server. The plugin Post Webhooks for Bitbucket
+     * https://marketplace.atlassian.com/plugins/nl.topicus.bitbucket.bitbucket-webhooks/server/overview
+     * should be installed and configured
+     *
+     * @param payload
+     */
+    private void processWebhookPayloadBitBucketServer(JSONObject payload) {
+        JSONObject repo = payload.getJSONObject("repository");
+        String user = payload.getJSONObject("actor").getString("username");
+        String url = "";
+        if (repo.getJSONObject("links").getJSONArray("self").size() != 0) {
+            try {
+                URL pushHref = new URL(repo.getJSONObject("links").getJSONArray("self").getJSONObject(0).getString("href"));
+                if (pushHref.getProtocol().equals("https")) {
+                    url = pushHref.toString().replaceFirst(new String("projects.*"), new String("scm/" + repo.getString("fullName").toLowerCase()));
+                } else {
+                    url = pushHref.toString().replaceFirst(new String("projects.*"), new String(repo.getString("fullName").toLowerCase()));
+                }
+                String scm = repo.has("scmId") ? repo.getString("scmId") : "git";
+                probe.triggerMatchingJobs(user, url, scm, payload.toString());
+            } catch (MalformedURLException e) {
+                LOGGER.log(Level.WARNING, String.format("URL %s is malformed", url), e);
+            }
+        }
     }
 
 /*
