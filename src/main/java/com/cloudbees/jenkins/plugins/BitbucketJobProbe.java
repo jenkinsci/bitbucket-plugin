860f8bd668c2fc3ff41f3787cde162be382051ec
tzachs <tzach.solomon@gmail.com>
2020-10-10 11:06:28 +0300

    BitbucketJobProbe - added logging for JENKINS-63848

--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketJobProbe.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketJobProbe.java
@@ -42,7 +42,7 @@ public class BitbucketJobProbe {
             SecurityContext old = Jenkins.getInstance().getACL().impersonate(ACL.SYSTEM);
             try {
                 URIish remote = new URIish(url);
-                for (Job<?,?> job : Jenkins.getInstance().getAllItems(Job.class)) {
+                for (Job<?, ?> job : Jenkins.getInstance().getAllItems(Job.class)) {
                     BitBucketTrigger bTrigger = null;
                     LOGGER.log(Level.FINE, "Considering candidate job {0}", job.getName());
 
@@ -58,11 +58,11 @@ public class BitbucketJobProbe {
                     if (bTrigger != null) {
                         LOGGER.log(Level.FINE, "Considering to poke {0}", job.getFullDisplayName());
                         SCMTriggerItem item = SCMTriggerItem.SCMTriggerItems.asSCMTriggerItem(job);
-                        if ( item == null){
+                        if (item == null) {
                             LOGGER.log(Level.INFO, "item is null");
                         } else {
                             List<SCM> scmTriggered = new ArrayList<>();
-                            if (item.getSCMs().isEmpty()){
+                            if (item.getSCMs().isEmpty()) {
                                 LOGGER.log(Level.WARNING, "No SCM configuration was found!");
                             }
                             for (SCM scmTrigger : item.getSCMs()) {
@@ -70,8 +70,9 @@ public class BitbucketJobProbe {
                                     LOGGER.log(Level.INFO, "Triggering BitBucket job {0}", job.getFullDisplayName());
                                     scmTriggered.add(scmTrigger);
                                     bTrigger.onPost(user, payload);
-                                } else
-                                    LOGGER.log(Level.FINE, "{0} SCM doesn't match remote repo {1}", new Object[]{job.getName(), remote});
+                                } else {
+                                    LOGGER.log(Level.FINEST, "{0} SCM doesn't match remote repo {1}", new Object[]{job.getName(), remote});
+                                }
                             }
                         }
                     } else
@@ -79,15 +80,17 @@ public class BitbucketJobProbe {
                 }
                 LOGGER.log(Level.FINE, "Now checking SCMSourceOwners/multiBranchProjects");
                 for (SCMSourceOwner scmSourceOwner : Jenkins.getInstance().getAllItems(SCMSourceOwner.class)) {
-                    LOGGER.log(Level.FINE, "Considering candidate scmSourceOwner {0}", scmSourceOwner);
+                    LOGGER.log(Level.FINE, "Considering candidate scmSourceOwner {0}", scmSourceOwner.getFullDisplayName());
                     List<SCMSource> scmSources = scmSourceOwner.getSCMSources();
                     for (SCMSource scmSource : scmSources) {
                         LOGGER.log(Level.FINER, "Considering candidate scmSource {0}", scmSource);
                         if (match(scmSource, remote)) {
                             LOGGER.log(Level.INFO, "Triggering BitBucket scmSourceOwner {0}", scmSourceOwner);
                             scmSourceOwner.onSCMSourceUpdated(scmSource);
-                        } else LOGGER.log(Level.FINE, "{0} SCM doesn't match remote repo {1}", new Object[]{scmSourceOwner.getName(), remote});
-                	}
+                        } else {
+                            LOGGER.log(Level.FINE, "{0} SCM doesn't match remote repo {1}", new Object[]{scmSourceOwner.getFullDisplayName(), remote});
+                        }
+                    }
                 }
             } catch (URISyntaxException e) {
                 LOGGER.log(Level.WARNING, "Invalid repository URL {0}", url);
@@ -111,28 +114,30 @@ public class BitbucketJobProbe {
 
     private boolean match(SCM scm, URIish url, String overrideUrl) {
         if (scm instanceof GitSCM) {
+            LOGGER.log(Level.FINE, "SCM is instance of GitSCM");
             for (RemoteConfig remoteConfig : ((GitSCM) scm).getRepositories()) {
                 for (URIish urIish : remoteConfig.getURIs()) {
                     // needed cause the ssh and https URI differs in Bitbucket Server.
-                    if(urIish.getPath().startsWith("/scm")){
+                    if (urIish.getPath().startsWith("/scm")) {
                         urIish = urIish.setPath(urIish.getPath().substring(4));
                     }
 
                     // needed because bitbucket self hosted does not transfer any host information
                     if (StringUtils.isEmpty(url.getHost())) {
-                    	urIish = urIish.setHost(url.getHost());
+                        urIish = urIish.setHost(url.getHost());
                     }
 
                     LOGGER.log(Level.FINE, "Trying to match {0} ", urIish.toString() + "<-->" + url.toString());
                     if (GitStatus.looselyMatches(urIish, url)) {
                         return true;
-                    } else if ( overrideUrl != null && !overrideUrl.isEmpty()){
+                    } else if (overrideUrl != null && !overrideUrl.isEmpty()) {
                         LOGGER.log(Level.FINE, "Trying to match using override Repository URL {0} ", overrideUrl + "<-->" + url.toString());
                         return overrideUrl.contentEquals(url.toString());
                     }
                 }
             }
         } else if (scm instanceof MercurialSCM) {
+            LOGGER.log(Level.FINEST, "SCM is instance of MercurialSCM");
             try {
                 URI hgUri = new URI(((MercurialSCM) scm).getSource());
                 String remote = url.toString();
@@ -142,28 +147,32 @@ public class BitbucketJobProbe {
             } catch (URISyntaxException ex) {
                 LOGGER.log(Level.SEVERE, "Could not parse jobSource uri: {0} ", ex);
             }
+        } else {
+            LOGGER.log(Level.FINEST, "SCM is instance of [" + scm.getClass().getSimpleName() + "] which is not supported");
         }
+
         return false;
     }
 
     private boolean match(SCMSource scm, URIish url) {
         if (scm instanceof GitSCMSource) {
-        	String gitRemote = ((GitSCMSource) scm).getRemote();
-        	URIish urIish;
-			try {
-				urIish = new URIish(gitRemote);
-			} catch (URISyntaxException e) {
-                LOGGER.log(Level.SEVERE, "Could not parse gitRemote: "+gitRemote, e);
+            LOGGER.log(Level.FINEST, "SCMSource is GitSCMSource");
+            String gitRemote = ((GitSCMSource) scm).getRemote();
+            URIish urIish;
+            try {
+                urIish = new URIish(gitRemote);
+            } catch (URISyntaxException e) {
+                LOGGER.log(Level.SEVERE, "Could not parse gitRemote: " + gitRemote, e);
                 return false;
-			}
+            }
             // needed cause the ssh and https URI differs in Bitbucket Server.
-            if(urIish.getPath().startsWith("/scm")){
+            if (urIish.getPath().startsWith("/scm")) {
                 urIish = urIish.setPath(urIish.getPath().substring(4));
             }
 
             // needed because bitbucket self hosted does not transfer any host information
             if (StringUtils.isEmpty(url.getHost())) {
-            	urIish = urIish.setHost(url.getHost());
+                urIish = urIish.setHost(url.getHost());
             }
 
             LOGGER.log(Level.FINE, "Trying to match {0} ", urIish.toString() + "<-->" + url.toString());
@@ -171,6 +180,7 @@ public class BitbucketJobProbe {
                 return true;
             }
         } else if (scm instanceof MercurialSCMSource) {
+            LOGGER.log(Level.FINEST, "SCMSource is MercurialSCMSource");
             try {
                 URI hgUri = new URI(((MercurialSCMSource) scm).getSource());
                 String remote = url.toString();
@@ -180,6 +190,8 @@ public class BitbucketJobProbe {
             } catch (URISyntaxException ex) {
                 LOGGER.log(Level.SEVERE, "Could not parse jobSource uri: {0} ", ex);
             }
+        } else {
+            LOGGER.log(Level.FINEST, "SCMSource is [" + scm.getClass().getSimpleName() + "] which is not supported");
         }
         return false;
     }
@@ -189,8 +201,8 @@ public class BitbucketJobProbe {
         try {
             URI repositoryUri = new URI(repository);
             result = Objects.equal(notifyUri.getHost(), repositoryUri.getHost())
-            && Objects.equal(notifyUri.getPath(), repositoryUri.getPath())
-            && Objects.equal(notifyUri.getQuery(), repositoryUri.getQuery());
+                    && Objects.equal(notifyUri.getPath(), repositoryUri.getPath())
+                    && Objects.equal(notifyUri.getQuery(), repositoryUri.getQuery());
         } catch (URISyntaxException ex) {
             LOGGER.log(Level.SEVERE, "Could not parse repository uri: {0}, {1}", new Object[]{repository, ex});
         }
